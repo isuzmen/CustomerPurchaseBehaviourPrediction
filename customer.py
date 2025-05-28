d775a00c8464f9badf0f797697032aad8157f171
İlyas SÜZMEN <73710864+isuzmen@users.noreply.github.com>
2025-05-28 19:50:31 +0300

    Handle missing and outlier values, create new features

--- customer.py
+++ customer.py
@@ -25,3 +25,17 @@ sns.set(style="whitegrid")
 
 df = pd.read_csv('marketing_campaign.csv', sep='\t')
 
+df['Income'] = df['Income'].fillna(df['Income'].median())
+
+Q95 = df['Income'].quantile(0.95)
+df['Income'] = np.where(df['Income'] > Q95, Q95, df['Income'])
+
+df['Age'] = 2023 - df['Year_Birth']
+df['Total_Children'] = df['Kidhome'] + df['Teenhome']
+df['Dt_Customer'] = pd.to_datetime(df['Dt_Customer'], format='%d-%m-%Y')
+df['Membership_Days'] = (df['Dt_Customer'].max() - df['Dt_Customer']).dt.days
+
+df['Marital_Status'] = df['Marital_Status'].replace({'Alone': 'Single', 'Absurd': 'Single', 'YOLO': 'Single'})
+df = pd.get_dummies(df, columns=['Education', 'Marital_Status'], drop_first=True)
+
+df.drop(columns=['Z_CostContact', 'Z_Revenue', 'ID', 'Year_Birth', 'Dt_Customer', 'Kidhome', 'Teenhome'], inplace=True)
